45767bea63f537c2acbcdd55a266c576adc3dc72
Claudia Prudente <claudiamary.santos@gmail.com>
2023-08-15 13:16:46 -0300

    feat: modificando complexidade para O(1)

--- stack/minStack/stack.ts
+++ stack/minStack/stack.ts
@@ -1,35 +1,39 @@
 export class Stack {
   items: number[];
   count: number;
+  minItem: number | undefined;
 
   constructor() {
     this.items = [];
     this.count = 0;
+    this.minItem = undefined;
   }
 
   push(value: number): void {
+    this.minItem = this.minItem ? Math.min(this.minItem, value) : value;
+
     this.items[this.count] = value;
     this.count++;
   }
 
   pop(): number | undefined {
+    if (this.count == 0) return undefined;
+    const poppedValue = this.items.pop();
+
+    this.minItem =
+      this.minItem === poppedValue ? Math.min(...this.items) : this.minItem;
+
     this.count--;
-    return this.items.pop();
+    return poppedValue;
   }
+
   top(): number | undefined {
     if (this.count == 0) return undefined;
 
     return this.items[this.count - 1];
   }
+
   getMin(): number | undefined {
-    if (this.count == 0) return undefined;
-
-    let minItem: number = this.items[0];
-
-    this.items.forEach((element) => {
-      minItem = minItem < element ? minItem : element;
-    });
-
-    return minItem;
+    return this.minItem;
   }
 }
