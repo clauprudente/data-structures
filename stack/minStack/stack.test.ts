c24bf6669aaaa81c5422890a1a7eabe135e7e265
Claudia Prudente <claudiamary.santos@gmail.com>
2023-08-15 13:17:03 -0300

    test: adicionando cenários de teste

--- stack/minStack/stack.test.ts
+++ stack/minStack/stack.test.ts
@@ -1,35 +1,55 @@
 import { Stack } from "./stack";
 
-describe("Stack", () => {
+describe("testing stack implementation", () => {
   let stack: Stack;
 
   beforeEach(() => {
     stack = new Stack();
   });
 
-  test("Teste de push e top", () => {
+  test("pushing stack", () => {
     stack.push(10);
     stack.push(14);
     stack.push(2);
     stack.push(20);
-    stack.top();
+
+    expect(stack).toEqual({ count: 4, items: [10, 14, 2, 20], minItem: 2 });
+  });
+
+  test("top stack", () => {
+    stack.push(10);
+    stack.push(14);
+    stack.push(2);
+    stack.push(20);
+
     expect(stack.top()).toBe(20);
   });
 
-  test("Teste de pop", () => {
+  test("pop stack", () => {
     stack.push(10);
     stack.push(14);
     stack.push(2);
     stack.push(20);
+
     expect(stack.pop()).toBe(20);
   });
 
-  test("Teste para getMin", () => {
+  test("get minimum value of stack", () => {
     stack.push(10);
     stack.push(14);
-    stack.push(2);
     stack.push(20);
-    stack.getMin();
+    stack.push(2);
+
     expect(stack.getMin()).toBe(2);
+  });
+
+  test("get minimum value of stack with pop", () => {
+    stack.push(10);
+    stack.push(14);
+    stack.push(20);
+    stack.push(2);
+    stack.pop();
+
+    expect(stack.getMin()).toBe(10);
   });
 });
