552d7fde4ae31f97b95551ba1da28ccc5067e20d
Claudia Prudente <claudiamary.santos@gmail.com>
2023-08-19 03:06:14 -0300

    fix: remoção console.log

--- list/double-linked-list/browser-history/browserHistory.ts
+++ list/double-linked-list/browser-history/browserHistory.ts
@@ -32,23 +32,15 @@ export class BrowserHistory {
 
   back(steps: number): string | void {
     if (steps >= this.history.size()) {
-      console.log("caiu no if");
       this.actualPosition = this.history.size();
     } else {
-      console.log("caiu no else");
       this.actualPosition += steps;
     }
     return this.history.get(this.actualPosition);
   }
 
   forward(steps: number): string | void {
-    console.log(
-      `this.history.size() : ${this.history.size()}  this.actualPosition: ${
-        this.actualPosition
-      }`
-    );
     if (steps <= this.history.size()) {
-      console.log("caiu no if");
       this.actualPosition -= steps;
     } else {
       this.actualPosition = 0;
@@ -56,14 +48,3 @@ export class BrowserHistory {
     return this.history.get(this.actualPosition);
   }
 }
-let browserHistory = new BrowserHistory();
-
-browserHistory.visit("/search");
-browserHistory.visit("/face");
-browserHistory.visit("/timeline");
-browserHistory.visit("/profile");
-browserHistory.visit("/dashboard");
-browserHistory.visit("/timeline2");
-
-console.log(browserHistory.back(4));
-console.log(browserHistory.forward(2));
