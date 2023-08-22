0c5f7176534ecdf03753d4b550a31a2c5886f83c
Claudia Prudente <claudiamary.santos@gmail.com>
2023-08-21 23:48:14 -0300

    feat: remove linked list element

---
+++ leetcode-exercises/203_remove-linked-list-elements.ts
@@ -0,0 +1,35 @@
+class ListNode {
+  val: number;
+  next: ListNode | null;
+
+  constructor(value?: number, next?: ListNode | null) {
+    this.val = value === undefined ? 0 : value;
+    this.next = next === undefined ? null : next;
+  }
+}
+
+function removeElements(head: ListNode | null, val: number): ListNode | null {
+  if (head == null) {
+    return null;
+  }
+
+  let currentNode: ListNode | null = head;
+  let previousNode: ListNode | null = null;
+
+  while (currentNode != null) {
+    if (currentNode.val === val) {
+      if (previousNode != null) {
+        previousNode.next = currentNode.next;
+      } else {
+        if (head.next) {
+          head = head?.next;
+        }
+      }
+    } else {
+      previousNode = currentNode;
+    }
+    currentNode = currentNode.next;
+  }
+
+  return head;
+}
