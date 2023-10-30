3ce85f054ffc85509923f6ab7ccad9d663ee83d5
claudia.santos <claudia.santos@infraclearsalecom.onmicrosoft.com>
2023-10-30 20:58:15 -0300

    adicionando complexidade e soma das vertices

--- structures/graph/kruskal.js
+++ structures/graph/kruskal.js
@@ -25,40 +25,44 @@ class Graph {
 function kruskal() {
   let mst = new Graph(v);
 
-  // passo 1: definir cada nó com uma cor diferente
+  // passo 1: definir cada nó com uma cor diferente O(V)
   let color = [];
   for (let i = 0; i < mst.vertices; i++) {
     color.push(1);
   }
 
-  //passo 2: criar um vetor com todas as arestas
+  //passo 2: criar um vetor com todas as arestas O(E)
   let edges = [];
   for (let source = 0; source < mst.vertices; source++) {
-    let numberOfNeighoors = mst.adjList[source].length;
+    let numberOfNeighbors = mst.adjList[source].length;
 
-    for (let i = 0; i < numberOfNeighoors; i++) {
+    for (let i = 0; i < numberOfNeighbors; i++) {
       let weight = mst.weightList[source][i];
       let target = mst.adjList[source][i];
 
       edges.push((weight, source, target));
     }
   }
-  //passo 3: ordenar o vetor de acordo com o peso
-  edges.sort();
+  //passo 3: ordenar o vetor de acordo com o peso O(E log E)
+  edges.sort((a) => a + b);
+  let sumEdges = 0;
 
-  //passo 4: iterar sobre o vetor ordenado
+  //passo 4: iterar sobre o vetor ordenado O(E*V)
   for (let i = 0; i < mst.edges.length; i++) {
     let weight = edges[i][0];
     let source = edges[i][1];
     let target = edges[i][2];
-
+    //passo 4.1: adiciona na solução aqueles com cores diferentes */
     if (color[source] != color[target]) {
+      //O(V-1)
       mst.addEdge(source, target, weight);
       mst.addEdge(target, source, weight);
+      sumEdges += weight;
 
-      colorSource = color[source];
+      let colorSource = color[source];
 
       for (let j = 0; j < color.length; j++) {
+        // O(V)
         if (color[j] === colorSource) {
           color[j] = color[target];
         }
@@ -66,7 +70,5 @@ function kruskal() {
     }
   }
 
-  //passo 4.1: adiciona na solução aqueles com cores diferentes */
-
-  return mst;
+  return { mst: mst, sumEdges: sumEdges };
 }
